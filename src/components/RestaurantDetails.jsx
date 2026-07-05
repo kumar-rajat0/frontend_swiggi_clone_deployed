3b02c8c590461226bd0961f6495b39995319ca84
kumar-rajat0 <rajatkumarpanipat2004@gmail.com>
2026-07-05 16:28:37 +0530

    fix restaurant menu issue

--- src/components/RestaurantDetails.jsx
+++ src/components/RestaurantDetails.jsx
@@ -1,54 +1,56 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { addItem } from "../utils/cartSlice"
-import { useDispatch } from "react-redux"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function RestaurantDetails(){
-    const[menuItems,setMenuItems] = useState([])
-    let {id} = useParams()
-       useEffect(() => {
-  async function calling() {
-      const API = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=29.69230&lng=76.98600&restaurantId=${id}&submitAction=ENTER`
-      const resp = await axios.get(API);
-      console.log(resp)
-      setMenuItems(resp.data.data.cards[5].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards)
-    } 
-  calling();
-}, [id]);
-    const dispatch = useDispatch()
-        function handleAddItem(foodItem){
-            dispatch(addItem(foodItem))
-        }
-  return (  
+function RestaurantDetails() {
+  const [menuItems, setMenuItems] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchMenu() {
+      try {
+        const API = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=29.69230&lng=76.98600&restaurantId=${id}`;
+
+        const resp = await axios.get(API);
+
+        const cards = resp.data?.data?.cards || [];
+
+        // 🔥 SAFE SEARCH (no fixed index)
+        const regularCard = cards.find(
+          (c) => c?.groupedCard?.cardGroupMap?.REGULAR
+        );
+
+        const itemCards =
+          regularCard?.groupedCard?.cardGroupMap?.REGULAR?.cards
+            ?.flatMap((c) => c?.card?.card?.itemCards || []) || [];
+
+        setMenuItems(itemCards);
+      } catch (err) {
+        console.log("Menu error:", err);
+        setMenuItems([]);
+      }
+    }
+
+    fetchMenu();
+  }, [id]);
+
+  return (
     <div>
-        <h1 className='text-center font-bold text-2xl'>List of item's avaialable at restaurant</h1>
-        {
-            menuItems.map((foodItem)=>{
-                return (
-                    <div className="flex w-3/4 mx-auto mb-10 border-b-4 p-4">
-                        <div className="flex flex-col w-3/4">
-                        <h1>{foodItem.card.info.name}</h1>
-                        <h1>Rs: {foodItem.card.info.price / 100}</h1>
-                        <h1>{foodItem.card.info.category}</h1>
-                        </div>
-                        <img
-                        className="w-52 h-44 rounded-md border shadow-lg border-gray-100"
-                        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${foodItem.card.info.imageId}`}
-                        alt=""
-                        />
+      <h1 className="text-center font-bold text-2xl">
+        Menu Items
+      </h1>
 
-                        <button
-                        className="border bg-green-300 h-8 relative top-16 right-5 focus:bg-red-700"
-                        onClick={()=>handleAddItem(foodItem)}
-                        >
-                        Add +
-                        </button>
-                    </div>
-                );
-            })
-        }
+      {menuItems.length === 0 ? (
+        <h2 className="text-center mt-10">No menu found</h2>
+      ) : (
+        menuItems.map((foodItem) => (
+          <div key={foodItem.card.info.id}>
+            <h2>{foodItem.card.info.name}</h2>
+          </div>
+        ))
+      )}
     </div>
-  )
+  );
 }
-export default RestaurantDetails
+
+export default RestaurantDetails;
